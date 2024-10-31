9173b4ef863ce8b7df7a946f3a161c801482ee64
youtheyeon <yuseoyeon@ewhain.net>
2024-10-31 19:12:17 +0900

    chore: 데이터 연결

--- week1/src/components/feed/Card.jsx
+++ week1/src/components/feed/Card.jsx
@@ -1,6 +1,6 @@
 import styles from "./Feed.module.css";
 
-const Card = () => {
+const Card = ({ item }) => {
   return (
     <li className={styles.card}>
       <div className={styles.card_img}>
@@ -8,26 +8,23 @@ const Card = () => {
       </div>
 
       <div className={styles.card_description}>
-        <h1>제 사이드 프로젝트 홍보 방법이요? OOO은 필수입니다.</h1>
-        <p>
-          2024년 첫 사이드 프로젝트 : 원영적 사고 변환기 출시 2주 만에 방문자
-          5만 달성 2024년 두 번째 사이드 프로젝트 : 내가 춘자라니 출시 한 달만에
-          방문자 60만 달성 2024년 세 번째 사이드 프로젝트 : 흑백요리사 테스트
-          출시 나흘만에 이용자 16만 달성
-        </p>
-        <span>2024년 10월 19일 · 19개의 댓글</span>
+        <h1>{item.title}</h1>
+        <p>{item.description}</p>
+        <span>
+          {item.date} · {item.comments}개의 댓글
+        </span>
       </div>
 
       <div className={styles.card_info}>
         <div>
           <i class="fa-solid fa-user"></i>
           <span>
-            <b>by</b> 혜욤이
+            <b>by</b> {item.author}
           </span>
         </div>
         <div>
           <i class="fa-solid fa-heart"></i>
-          <span>113</span>
+          <span>{item.likes}</span>
         </div>
       </div>
     </li>
--- week1/src/components/feed/Feed.jsx
+++ week1/src/components/feed/Feed.jsx
@@ -1,29 +1,37 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import styles from "./Feed.module.css";
+import mockData from "../../assets/data.json";
 
 const Feed = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(mockData);
+  }, []);
+
   return (
     <main className={styles.feed_container}>
       <div className={styles.feed_category_container}>
         <ul className={styles.feed_category}>
           <li>
-            <i class="fa-solid fa-arrow-trend-up"></i>트렌딩
+            <i className="fa-solid fa-arrow-trend-up"></i>트렌딩
           </li>
           <li>
-            <i class="fa-regular fa-clock"></i>최신
+            <i className="fa-regular fa-clock"></i>최신
           </li>
           <li>
-            <i class="fa-solid fa-rss"></i>피드
+            <i className="fa-solid fa-rss"></i>피드
           </li>
         </ul>
 
-        <i class="fa-solid fa-ellipsis-vertical"></i>
+        <i className="fa-solid fa-ellipsis-vertical"></i>
       </div>
 
       <ul className={styles.feed}>
-        <Card />
-        <Card />
-        <Card />
+        {data.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
       </ul>
     </main>
   );
