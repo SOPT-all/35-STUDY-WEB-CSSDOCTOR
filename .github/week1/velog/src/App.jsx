def27afd6c25f8776a94914dee96d5388f52fe10
bongtta <kce0419@naver.com>
2024-11-06 11:36:05 +0900

    init: 초기 세팅

---
+++ .github/week1/velog/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+    </>
+  )
+}
+
+export default App
