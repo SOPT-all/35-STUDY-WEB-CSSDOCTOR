041de0c04004206fc075a027151fb8a1f5c1d50f
imddoy <imddoy@duksung.ac.kr>
2024-12-06 20:06:16 +0900

    design: 헤더, 사이드바 위치 고정

--- week3_KimChaeHyun/src/components/Content.jsx
+++ week3_KimChaeHyun/src/components/Content.jsx
@@ -4,13 +4,13 @@ import Product from "./product/Product";
 
 const Content = () => {
   return (
-    <>
+    <div className={styles.content}>
       <h1 className={styles.title}>전체</h1>
       <div className={styles.container}>
         <Sidebar />
         <Product />
       </div>
-    </>
+    </div>
   );
 };
 
